4e48734dacfff374aac3b9aebfa17bec6d7b5368
pythonlessons <pythonlessons0@gmail.com>
2022-09-09 16:24:20 +0300

    MPSegmentations -> MPSegmentation

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from utils import FPSmetric
-from selfieSegmentation import MPSegmentations
+from selfieSegmentation import MPSegmentation
 from engine import Engine
 
 if __name__ == '__main__':
     fpsMetric = FPSmetric()
-    segmentationModule = MPSegmentations(threshold=0.3, bg_images_path='', bg_blur_ratio=(45, 45))
+    segmentationModule = MPSegmentation(threshold=0.3, bg_images_path='', bg_blur_ratio=(45, 45))
     selfieSegmentation = Engine(webcam_id=0, show=True, custom_objects=[segmentationModule, fpsMetric])
     selfieSegmentation.run()
