40355f592cb7c642355a458921dafa8270e8e37d
pythonlessons <pythonlessons0@gmail.com>
2022-09-15 16:47:34 +0300

    Update FaceDetection doce

--- README.md
+++ README.md
@@ -25,5 +25,19 @@ if __name__ == '__main__':
 ```
 You can run it by typing ```python main.py``` in a terminal.
 
+## Run basic MediaPipe face detection:
+```Python
+from utils import FPSmetric
+from faceDetection import MPFaceDetection
+from engine import Engine
+
+if __name__ == '__main__':
+    fpsMetric = FPSmetric()
+    mpFaceDetector = MPFaceDetection() 
+    selfieSegmentation = Engine(webcam_id=0, show=True, custom_objects=[mpFaceDetector, fpsMetric])
+    selfieSegmentation.run()
+```
+You can run it by typing ```python main.py``` in a terminal.
+
 ## Detailed Tutorials:
-- First One
+- [Selfie background remove or blur with Python](https://pylessons.com/remove-background)
--- faceDetection.py
+++ faceDetection.py
@@ -1,8 +1,6 @@
 import numpy as np
 import mediapipe as mp
 
-mp_drawing = mp.solutions.drawing_utils
-
 class MPFaceDetection:
     """Object to create and do mediapipe face detection, more about it:
     https://google.github.io/mediapipe/solutions/face_detection.html
@@ -15,8 +13,9 @@ class MPFaceDetection:
         """
         Args:
             model_selection: (bool) - 1 - for low distance, 0 - for far distance face detectors
-            confidence: (float) - confidence for face detector, when detection are confirmed
+            confidence: (float) - confidence for face detector, when detection are confirmed, range (0.0-1.0)
         """
+        self.mp_drawing = mp.solutions.drawing_utils
         self.mp_face_detection = mp.solutions.face_detection
         self.face_detection = self.mp_face_detection.FaceDetection(model_selection=model_selection, min_detection_confidence=confidence)
 
@@ -28,13 +27,13 @@ class MPFaceDetection:
             frame: (np.ndarray) - frame to excecute face detection on
 
         Returns:
-            frame: (np.ndarray) - processed frame with face detection
+            frame: (np.ndarray) - processed frame with detected faces
         """
         results = self.face_detection.process(frame)
 
         if results.detections:
             # Draw face detections of each face.
             for detection in results.detections:
-                mp_drawing.draw_detection(frame, detection)
+                self.mp_drawing.draw_detection(frame, detection)
  
         return frame
