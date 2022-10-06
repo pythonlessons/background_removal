331b4311206f0580418b76a28177f08c4596292d
pythonlessons <pythonlessons0@gmail.com>
2022-10-06 13:35:12 +0300

    FaceNet object for face identification

---
+++ faceNet/convert_to_onnx.py
@@ -0,0 +1,19 @@
+import os
+import tf2onnx
+from architecture import InceptionResNetV2
+
+if __name__ == '__main__':
+    """ weights can be downloaded from https://drive.google.com/drive/folders/1scGoVCQp-cNwKTKOUqevCP1N2LlyXU3l?usp=sharing
+    Put facenet_keras_weights.h5 file in model folder
+    """
+    facenet_weights_path = "models/facenet_keras_weights.h5"
+    onnx_model_output_path = "models/faceNet.onnx"
+
+    if not os.path.exists(facenet_weights_path):
+        raise Exception(f"Model doesn't exists in {facenet_weights_path}, download weights from \
+            https://drive.google.com/drive/folders/1scGoVCQp-cNwKTKOUqevCP1N2LlyXU3l?usp=sharing")
+
+    faceNet = InceptionResNetV2()
+    faceNet.load_weights(facenet_weights_path) 
+
+    tf2onnx.convert.from_keras(faceNet, output_path=onnx_model_output_path)
