6cc784c59a9155badb1df789967fcd9a706d609a
pythonlessons <pythonlessons0@gmail.com>
2022-11-13 18:38:32 +0200

    husky and onnx models

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-from utils import FPSmetric
 from engine import Engine
-from faceDetection import MPFaceDetection
-from faceNet.faceNet import FaceNet
+from animegan import AnimeGAN
 
 if __name__ == '__main__':
-    facenet = FaceNet(
-        detector = MPFaceDetection(),
-        onnx_model_path = "models/faceNet.onnx", 
-        anchors = "faces",
-        force_cpu = True,
-    )
-    engine = Engine(webcam_id=0, show=True, custom_objects=[facenet, FPSmetric()])
-
-    # save first face crop as anchor, otherwise don't use
-    while not facenet.detect_save_faces(engine.process_webcam(return_frame=True), output_dir="faces"):
-        continue
-
+    animegan = AnimeGAN("models/Hayao_64.onnx")
+    engine = Engine(webcam_id=0, show=True, custom_objects=[animegan])
     engine.run()
