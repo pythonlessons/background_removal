9e1008dfbb30e9323d8dfa60bf675be62cdc0db9
domkas <kasdomantas@gmail.com>
2023-05-03 17:32:38 +0300

    show change

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import argparse
+import threading
 from utils import FPSmetric
 from engine import Engine
 from faceDetection import MPFaceDetection
 from faceNet.faceNet import FaceNet
 from locker import Locker
+
+def update_show(engine):
+    while True:
+        input_str = input('Enter "s" to toggle camera feed on/off: ')
+        if input_str.lower() == 's':
+            engine.show = not engine.show
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -19,5 +26,9 @@ if __name__ == '__main__':
         force_cpu=True,
     )
     engine = Engine(webcam_id=0, show=args.show, custom_objects=[facenet, FPSmetric()])
+
+    show_thread = threading.Thread(target=update_show, args=(engine,))
+    show_thread.daemon = True
+    show_thread.start()
 
     engine.run()
